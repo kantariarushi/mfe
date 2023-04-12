754d9234805652274a61966d66a0a8d057d222d5
rushikantaria@gmail.com <rushikantaria@gmail.com>
2023-04-12 10:54:04 +0530

    initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App';
+
+//Mount function to start up the app
+const mount = (el) => {
+    ReactDOM.render(<App/>,el);
+};
+
+//if we are in development and in isolation,
+//call mount immdiately
+if(process.env.NODE_ENV === 'development'){
+    const devRoot = document.querySelector('#_marketing-dev-root');
+    if(devRoot){
+        mount(devRoot);
+    }
+}
+
+//we are running through contaniner
+//and we should export the mount function
+export { mount };
